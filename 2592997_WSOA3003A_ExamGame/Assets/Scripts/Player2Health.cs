832c43f4e15f70d07aa1e4c24a45ce0b9fb8a357
Amro <2592997@students.wits.ac.za>
2024-06-11 14:08:55 +0200

    Ok

--- 2592997_WSOA3003A_ExamGame/Assets/Scripts/Player2Health.cs
+++ 2592997_WSOA3003A_ExamGame/Assets/Scripts/Player2Health.cs
@@ -8,6 +8,12 @@ public class Player2Health : MonoBehaviour
     public float HealthPoints = 200f;
     public Animator animator;
     public Slider HealthBar;
+    public GunController gunController;
+    public Player2GunController Player2gunController;
+    public Camera WinnerCamera;
+    public Camera LoserCamera;
+    public GameObject Canvas1;
+    public GameObject Canvas2;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +26,12 @@ public class Player2Health : MonoBehaviour
         if (HealthPoints <= 0)
         {
             animator.SetBool("IsDying", true);
+            gunController.enabled = false;
+            Player2gunController.enabled = false;
+            WinnerCamera.rect = new Rect(0f, 0f, 1f, 1f);
+            LoserCamera.enabled = false;
+            Canvas1.SetActive(false);
+            Canvas2.SetActive(false);
         }
         HealthBar.value = HealthPoints;
     }
